abadc0c3419dcd9674de6c0d270f232174a89eda
dileeptesting <66291889+dileeptesting@users.noreply.github.com>
2020-06-04 08:29:17 +0530

    Added Login Button Code
    Added Login button code and executed  the same. Deleted extra method.

--- Move_AMS/src/LoginPage.java
+++ Move_AMS/src/LoginPage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginPage {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		
 		//Path setup for chromedriver executable file
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Dell\\Downloads\\chromedriver_win32\\chromedriver.exe");
@@ -12,8 +12,9 @@ public class LoginPage {
 		//Create and instance of chromedriver class and store it in a object(move) and launch browser
 		WebDriver move = new ChromeDriver();
 		
+		//Maximize chrome browser
 		move.manage().window().maximize();
-		//Disconnect team viewr and share your screen in skype ok? ok
+
 		//Open the move ams Url
 		move.get("https://ams.moveoutdoor.com.au/");
 		
@@ -22,24 +23,17 @@ public class LoginPage {
 		
 		//Enter text in the email id field
 		move.findElement(By.id("contact_email")).sendKeys("dileep@gmail.com");
+		
 		//Enter text in the password field
 		move.findElement(By.id("contact_password")).sendKeys("Dileep@12");
 		
-		//Click on login button
-		//move.select
-		
-		//sleep time 
-	     move.wait(3);
+		//Click login button
+		move.findElement(By.name("commit")).click();
 		
 		//Should close the Browser
 		move.close();
 		
 		System.out.println("Launch browser, maximize browser, enter url, close pop-up, enter email-id, password and close browser");
-	}
-
-	private static void close() {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
