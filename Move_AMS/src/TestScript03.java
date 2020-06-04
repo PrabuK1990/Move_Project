9328e9a78eee86c7789a0b5da89fd9738f05ecc3
dileeptesting <66291889+dileeptesting@users.noreply.github.com>
2020-06-04 11:40:06 +0530

    Added TestScripts
    Added Reusable Test Script03, TestScript04 and Run Sucessfully

--- Move_AMS/src/TestScript03.java
+++ Move_AMS/src/TestScript03.java
@@ -2,6 +2,17 @@
 public class TestScript03 {
 
 	public static void main(String[] args) {
+		
+		LoginPage test03 = new LoginPage();
+		test03.setPath();
+		test03.chromeBrowser();
+		test03.maximizeWindow();
+		test03.moveUrl();
+		test03.browserPopup();
+		test03.passwordField();
+		test03.loginButton();
+		test03.closeBrowser();	
+		
 		System.out.println("Scenario 3: Launch browser, maximize browser, enter url, close pop-up, enter password, click login button and close browser");
 		
 
--- Move_AMS/src/TestScript04.java
+++ Move_AMS/src/TestScript04.java
@@ -2,6 +2,17 @@
 public class TestScript04 {
 
 	public static void main(String[] args) {
+		LoginPage test04 =  new LoginPage();
+		test04.setPath();
+		test04.chromeBrowser();
+		test04.maximizeWindow();
+		test04.moveUrl();
+		test04.browserPopup();
+		test04.emailField();
+		test04.passwordField();
+		test04.loginButton();
+		test04.closeBrowser();
+		
 		System.out.println("Scenario 4: Launch browser, maximize browser, enter url, close pop-up, enter email-id, enter password, click login button and close browser");
 		
 
